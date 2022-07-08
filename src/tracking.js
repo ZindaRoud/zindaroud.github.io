a9ae31600890004270d085d24e7ab199775d15cd
ZindaRoud <2310250@lums.edu.pk>
2022-07-08 15:51:22 +0500

    idk

---
+++ src/tracking.js
@@ -0,0 +1,24 @@
+import React from "react";
+
+const Tracking = () => {
+  return (
+    <div>
+      <script
+        async
+        src="https://www.googletagmanager.com/gtag/js?id=G-61N8F5ZKMV"
+      ></script>
+      <script>
+        {() => {
+          window.dataLayer = window.dataLayer || [];
+          function gtag() {
+            dataLayer.push(arguments);
+          }
+          gtag("js", new Date());
+          gtag("config", "G-61N8F5ZKMV");
+        }}
+      </script>
+    </div>
+  );
+};
+
+ReactDOM.render(<Tracking />, document.querySelector("#tracking"));
